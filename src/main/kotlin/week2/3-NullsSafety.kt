3723a1c69a5aadc1d347edbf45aa1eeaa62dad7a
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-05-09 18:19:00 -0300

    Reestruturação e Exemplos Semana 2.

---
+++ src/main/kotlin/week2/3-NullsSafety.kt
@@ -0,0 +1,18 @@
+package week2
+
+fun main() {
+
+    val s1: String = "abc"
+    val s2: String? = null
+
+    s1.length
+    // s2.length
+
+    var s2Length = if (s2 != null) s2.length else 0
+    s2Length = s2?.length ?: 0
+
+    if (s2 != null) {
+        println(s2.length) // smart cast
+    }
+
+}
